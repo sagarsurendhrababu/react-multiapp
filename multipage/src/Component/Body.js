e5efa51751561aeb9e06e6a8ef3b2241f09d1605
sagar <sagarsurendhrababau@gmail.com>
2021-12-31 04:52:18 +0530

    created one page application

---
+++ multipage/src/Component/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Body.css';
+
+const Body = () => {
+    return (
+        <div className="col12 body">
+            <h3>Body</h3>
+        </div>
+    )
+}
+
+export default Body;
